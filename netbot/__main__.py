ef4a5bfdc4431d73a8dd459aaf99c03a68808252
williamabreu <contato@williamabreu.net>
2020-07-30 16:53:39 -0300

    Add main module for testing mikrotik api
    - Get identity
    - Get routerboard model
    - Print in CSV style

--- netbot/__main__.py
+++ netbot/__main__.py
@@ -1,5 +1,30 @@
+# import librouteros
+# import telnetlib
+# import paramiko
+# import json
+from netbot.keys import Keys
+from netbot.mikrotik.Routerboard import Routerboard
+
+
 def main():
-    print('Hello world!')
+    hosts = [
+        # '192.168.56.45',
+        # '192.168.56.42',
+        # '192.168.56.34',
+        '172.16.240.49',
+    ]
+
+    for ip in hosts:
+        rb = Routerboard(ip)
+        rb.api_connect(Keys.username, Keys.password)
+        print(ip, rb.get_identity(), rb.get_routerboard()['model'], sep=',')
+        rb.api_disconnect()
+
+    # SSH
+    # ssh = paramiko.SSHClient()
+    # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # ssh.connect(hostname='192.168.56.30', username='admin', password='')
+    
 
 if __name__ == '__main__':
     main()
