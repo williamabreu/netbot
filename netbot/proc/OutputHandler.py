0e57b9920fd79ab6d644793fa8c3d318f72c501d
williamabreu <contato@williamabreu.net>
2020-08-05 15:16:05 -0300

    Add high level output handler

---
+++ netbot/proc/OutputHandler.py
@@ -0,0 +1,21 @@
+class OutputHandler:
+    __INSTANCE = None
+
+    def __init__(self):
+        self.__cached_data = []
+
+    @staticmethod
+    def get_instance():
+        if OutputHandler.__INSTANCE is None:
+            OutputHandler.__INSTANCE = OutputHandler()
+        return OutputHandler.__INSTANCE
+    
+    def register(self, *args):
+        self.__cached_data.append(','.join(map(str, args)))
+
+    def save(self):
+        with open('RESULT.csv', 'w') as fp:
+            fp.write('IP,IDENTITY,MODEL\n')
+            for line in self.__cached_data:
+                fp.write(line + '\n')
+                
