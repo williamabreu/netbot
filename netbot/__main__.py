3f5b68764e2491cfd52b21fb11dcf857955e0aff
williamabreu <contato@williamabreu.net>
2020-08-05 15:17:57 -0300

    Update main module to use output handler

--- netbot/__main__.py
+++ netbot/__main__.py
@@ -3,21 +3,57 @@
 # import paramiko
 # import json
 from netbot.mikrotik.Worker import MikrotikWorker
+from netbot.zabbix.Zabbix import Zabbix
+from netbot.proc.OutputHandler import OutputHandler
+
+
+# Number of threads:
+N_THREADS = 4
+
+
+# Thread load balance:
+def balance(length):
+    slices = []
+    last = 0
+    num_elements = length
+    num_threads = N_THREADS
+    for i in range(N_THREADS):
+        part_size = round(num_elements / (num_threads - i))
+        slices.append(slice(last, last + part_size))
+        num_elements -= part_size
+        last += part_size
+    return slices
 
 
 def main():
-    hosts = [
-        # '192.168.56.45',
-        # '192.168.56.42',
-        # '192.168.56.34',
-        '172.16.240.1',
-        '177.93.103.102',
-        '172.16.240.4',
-    ]
+    zabbix_hosts = None
+    
+    with open('zabbix_hosts.json') as fp:
+        zabbix_hosts = json.load(fp)
+    
+    hosts = [host['ip'] for host in zabbix_hosts]
+    slices = balance(len(hosts))
+    threads = []
+    print(len(slices))
+    
+    for interval in slices:
+        worker = MikrotikWorker(hosts[interval])
+        worker.start()
+        threads.append(worker)
+    
+    for thread in threads:
+        thread.join()
+    
+    output = OutputHandler.get_instance()
+    output.save()
+    
+    
 
-    worker1 = MikrotikWorker(hosts)
-    worker1.start()
-
+    # zabbix = Zabbix('http://netmon.minasvip.com.br')
+    # zabbix.api_connect('monitoramento', '@minas2015')
+    # x = zabbix.get_monitored_hosts()
+    # zabbix.api_disconnect()
+    # print(json.dumps(x, indent=4))
 
     # SSH
     # ssh = paramiko.SSHClient()
