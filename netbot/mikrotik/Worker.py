f84d90750dfca3728d05a87890753b62b42e0c9e
williamabreu <contato@williamabreu.net>
2020-08-05 15:17:39 -0300

    Update worker to use output handler

--- netbot/mikrotik/Worker.py
+++ netbot/mikrotik/Worker.py
@@ -1,19 +1,21 @@
 import threading
 from netbot.keys import Keys
 from netbot.mikrotik.Routerboard import Routerboard
+from netbot.proc.OutputHandler import OutputHandler
 
 
 class MikrotikWorker(threading.Thread):
     def __init__(self, target_hosts):
         super().__init__()
         self.__hosts = target_hosts
+        self.__output = OutputHandler.get_instance()
 
     def run(self):
         for ip in self.__hosts:
             try:
                 rb = Routerboard(ip)
                 rb.api_connect(Keys.username, Keys.password)
-                print(ip, rb.get_identity(), rb.get_routerboard()['model'], sep=',')
+                self.__output.register(ip, rb.get_identity(), rb.get_routerboard()['model'])
                 rb.api_disconnect()
             except ConnectionError as e:
-                print(ip, 'ERROR', e, sep=',')
+                self.__output.register(ip, 'ERROR', e)
